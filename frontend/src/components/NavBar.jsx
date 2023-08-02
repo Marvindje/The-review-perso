f0c3998d0f9f5a703de57aad013720dc3883b853
Marvindje <marvindje@wilder.school>
2023-08-02 14:03:58 +0200

    ajout menu burger

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/the-review-high-resolution-logo-color-on-transparent-background.png";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="bg-white shadow">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -15,7 +17,22 @@ function Navbar() {
               <h1 className="font-semibold text-xl">The Review</h1>
             </Link>
           </div>
-          <div className="flex sm:block sm:ml-6">
+          <div className="flex sm:hidden">
+            <button type="button" onClick={() => setIsOpen(!isOpen)}>
+              <svg
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="menu w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2 4.5A1.5 1.5 0 013.5 3h13a1.5 1.5 0 010 3h-13A1.5 1.5 0 012 4.5zm0 5A1.5 1.5 0 013.5 8h13a1.5 1.5 0 010 3h-13A1.5 1.5 0 012 9.5zm1.5 5a1.5 1.5 0 000 3h13a1.5 1.5 0 000-3h-13z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className={`sm:flex sm:items-center sm:ml-6 ${isOpen ? "flex" : "hidden"} flex-col sm:flex-row`}>
             <div className="flex space-x-4">
               <input
                 type="text"
