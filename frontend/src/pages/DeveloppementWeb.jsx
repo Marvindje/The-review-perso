08c60492c7c239c9b044a084566db81d51b0b6d6
Marvindje <marvindje@wilder.school>
2023-08-03 22:38:03 +0200

    ajout des heor pour les categories

--- frontend/src/pages/DeveloppementWeb.jsx
+++ frontend/src/pages/DeveloppementWeb.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import heroImage from '../assets/dev-web.jpeg';// Remplacez par votre image
- // Remplacez par votre image
+import devWebImage from '../assets/dev-web.jpeg';
 
 const DeveloppementWeb = () => {
   return (
     <div className="font-body">
       {/* Hero section */}
-      <div className="relative h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${heroImage})` }}>
-        <h1 className="text-5xl text-white font-header text-center">Bienvenue dans la section Développement Web</h1>
+      <div className="relative h-screen flex items-center justify-center bg-cover bg-center shadow-2xl" style={{ backgroundImage: `url(${devWebImage})` }}>
+        <h1 className="text-5xl text-white font-header text-center bg-gradient-to-r from-black to-transparent py-2 px-4 rounded hover:text-blue-500 transition-colors duration-200">
+          Bienvenue dans la section Développement Web
+        </h1>
       </div>
 
-      {/* Articles section */}
-      <div className="container mx-auto px-6 py-8">
-        <h2 className="text-4xl font-header text-center mb-8">Découvrez nos derniers articles sur le développement web</h2>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* Remplacez cette partie par une boucle pour afficher vos articles */}
-          <div className="bg-white rounded-lg overflow-hidden shadow-lg">
-            <img className="h-56 w-full object-cover" src={articleImage} alt="Article" />
-            <div className="p-6">
-              <h3 className="text-2xl font-header mb-2">Titre de l'article</h3>
-              <p className="text-gray-700 text-sm">Description de l'article...</p>
-              <Link to="/article" className="text-blue-500 text-sm mt-4 block">Lire l'article</Link>
+      <div className="flex justify-center items-center mt-64 mb-20">
+        <div className="grid grid-cols-2 gap-6">
+          {/* Replace this part with a loop to display your articles */}
+          {[...Array(4)].map((_, i) => (
+            <div key={i} className="bg-gradient-to-r from-blue-500 to-cyan-500 rounded-lg overflow-hidden shadow-lg p-6 hover:bg-blue-500 hover:text-white transition-colors duration-200">
+              <h3 className="text-2xl font-header mb-2">Titre de l'article {i+1}</h3>
+              <p className="text-white text-sm">Description de l'article...</p>
+              <Link to={`/article${i+1}`} className="text-white text-sm mt-4 block hover:text-white">Lire l'article</Link>
             </div>
-          </div>
-          {/* Fin de la boucle */}
+          ))}
+          {/* End of the loop */}
         </div>
       </div>
     </div>
--- frontend/src/pages/Hardware.jsx
+++ frontend/src/pages/Hardware.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import hardwareImage from '../assets/hardware3.jpeg';
 
 const Hardware = () => {
   return (
-    <div>
-      {/* Your code for the Hardware page here */}
+    <div className="font-body">
+
+      <div className="relative h-screen flex items-center justify-center bg-cover bg-center shadow-2xl" style={{ backgroundImage: `url(${hardwareImage})` }}>
+        <h1 className="text-5xl text-white font-header text-center bg-gradient-to-r from-black to-transparent py-2 px-4 rounded hover:text-blue-500 transition-colors duration-200">
+          Bienvenue dans la section Hardware
+        </h1>
+      </div>
+
+      <div className="flex justify-center items-center mt-64 mb-20">
+        <div className="grid grid-cols-2 gap-6">
+         
+          {[...Array(4)].map((_, i) => (
+            <div key={i} className="bg-gradient-to-r from-blue-500 to-cyan-500 rounded-lg overflow-hidden shadow-lg p-6 hover:bg-blue-500 hover:text-white transition-colors duration-200">
+              <h3 className="text-2xl font-header mb-2">Titre de l'article {i+1}</h3>
+              <p className="text-white text-sm">Description de l'article...</p>
+              <Link to={`/article${i+1}`} className="text-white text-sm mt-4 block hover:text-white">Lire l'article</Link>
+            </div>
+          ))}
+   
+        </div>
+      </div>
     </div>
   );
 };
