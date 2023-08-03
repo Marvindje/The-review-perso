249108117bdebcb97f79ceb1d151e04c852f4775
Marvindje <marvindje@wilder.school>
2023-08-03 15:39:10 +0200

    navbar amelioration

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { FaSearch } from "react-icons/fa";
-import logo from "../assets/the-review-website-favicon-color.png";
+import logo from "../assets/logotheview.png";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,19 +12,27 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <motion.img 
-              className="h-10 w-auto" 
-              src={logo} 
-              alt="Workflow" 
-              initial={{ y: -250 }}
-              animate={{ y: -10 }}
-              transition={{ type: 'spring', stiffness: 120 }}
-            />
+            <Link to="/">
+              <motion.img 
+                className="h-10 w-auto" 
+                src={logo} 
+                alt="Workflow" 
+                initial={{ y: -250 }}
+                animate={{ y: -10 }}
+                transition={{ type: 'spring', stiffness: 120 }}
+              />
+            </Link>
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-center">
-            <Link to="/">
-              <h1 className="font-semibold text-xl">The Review</h1>
-            </Link>
+            <div className="relative">
+              <FaSearch className="absolute left-3 top-3 h-5 w-5 text-gray-500" />
+              <motion.input
+                type="text"
+                placeholder="Search..."
+                className="pl-10 pr-3 py-2 rounded-md text-sm font-medium"
+                whileFocus={{ scale: 1.1 }}
+              />
+            </div>
           </div>
           <div className="flex sm:hidden">
             <button type="button" onClick={() => setIsOpen(!isOpen)}>
@@ -43,15 +51,7 @@ function Navbar() {
           </div>
           <div className={`sm:flex sm:items-center sm:ml-6 ${isOpen ? "flex" : "hidden"} flex-col sm:flex-row`}>
             <div className="flex space-x-4">
-              <div className="relative">
-                <FaSearch className="absolute left-3 top-3 h-5 w-5 text-gray-500" />
-                <motion.input
-                  type="text"
-                  placeholder="Search..."
-                  className="pl-10 pr-3 py-2 rounded-md text-sm font-medium"
-                  whileFocus={{ scale: 1.1 }}
-                />
-              </div>
+            
               <Link
                 to="/"
                 className="text-gray-700 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
