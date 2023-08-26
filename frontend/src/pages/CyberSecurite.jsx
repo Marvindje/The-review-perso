07fec9e14751436fc9e40acdb075e12969bdbd03
Marvindje <marvindje@wilder.school>
2023-08-26 13:05:56 +0200

    heor hovers

--- frontend/src/pages/CyberSecurite.jsx
+++ frontend/src/pages/CyberSecurite.jsx
@@ -1,8 +1,7 @@
 // CyberSecurite.jsx
 import React from 'react';
-import ArticleSection from '../components/ArticleSection';
-// Replace with your actual image
 import cyberSecuriteImage from '../assets/cyber-secu.jpeg';
+import ArticleSection from '../components/ArticleSection';
 
 const CyberSecurite = () => {
   return <ArticleSection title="Cyber Sécurité" image={cyberSecuriteImage} />;
