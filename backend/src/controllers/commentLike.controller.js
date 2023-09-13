51e7b0c65130d8cc4d7f5aa8e381500632a6fda2
Marvindje <marvindje@wilder.school>
2023-09-13 11:06:32 +0200

    ajout des methodes manquantes pour les controllers like et comment like et ajout de filtrages

--- backend/src/controllers/commentLike.controller.js
+++ backend/src/controllers/commentLike.controller.js
@@ -40,6 +40,56 @@ class CommentLikeController {
       res.status(500).send({ error: err.message });
     }
   }
+
+   // Ici, je récupère tous les commentLikes de la base de données.
+   static async findAll(req, res) {
+    try {
+      const commentLikes = await CommentLikeModel.findAll();
+      res.status(200).send(commentLikes);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: err.message });
+    }
+  }
+
+  // Ici, je récupère un commentLike spécifique en utilisant son ID.
+  static async findById(req, res) {
+    try {
+      const { commentLikeId } = req.params;
+      const commentLike = await CommentLikeModel.findByPk(commentLikeId);
+      if (!commentLike) {
+        return res.status(404).send(`CommentLike (${commentLikeId}) not found!`);
+      }
+      res.status(200).send(commentLike);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: err.message });
+    }
+  }
+
+  // Ici, je récupère tous les commentLikes pour un utilisateur spécifique.
+  static async findByUserId(req, res) {
+    try {
+      const { userId } = req.params;
+      const commentLikes = await CommentLikeModel.findAll({ where: { userId } });
+      res.status(200).send(commentLikes);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: err.message });
+    }
+  }
+
+  // Ici, je récupère tous les commentLikes pour un commentaire spécifique.
+  static async findByCommentId(req, res) {
+    try {
+      const { commentId } = req.params;
+      const commentLikes = await CommentLikeModel.findAll({ where: { commentId } });
+      res.status(200).send(commentLikes);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: err.message });
+    }
+  }
 }
 
 module.exports = CommentLikeController;
--- backend/src/controllers/like.controller.js
+++ backend/src/controllers/like.controller.js
@@ -40,6 +40,80 @@ class LikeController {
       res.status(500).send({ error: err.message });
     }
   }
+
+  static async findAll(req, res) {
+    try {
+      const likes = await LikeModel.findAll();
+      res.status(200).send(likes);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: err.message });
+    }
+  }
+
+  static async findById(req, res) {
+    try {
+      const { likeId } = req.params;
+      const like = await LikeModel.findByPk(likeId);
+      if (!like) {
+        return res.status(404).send(`Like (${likeId}) not found!`);
+      }
+      res.status(200).send(like);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: err.message });
+    }
+  }
+
+   // Ici, je récupère tous les likes de la base de données.
+   static async findAll(req, res) {
+    try {
+      const likes = await LikeModel.findAll();
+      res.status(200).send(likes);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: err.message });
+    }
+  }
+
+  // Ici, je récupère un like spécifique en utilisant son ID.
+  static async findById(req, res) {
+    try {
+      const { likeId } = req.params;
+      const like = await LikeModel.findByPk(likeId);
+      if (!like) {
+        return res.status(404).send(`Like (${likeId}) not found!`);
+      }
+      res.status(200).send(like);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: err.message });
+    }
+  }
+
+  // Ici, je récupère tous les likes pour un utilisateur spécifique.
+  static async findByUserId(req, res) {
+    try {
+      const { userId } = req.params;
+      const likes = await LikeModel.findAll({ where: { userId } });
+      res.status(200).send(likes);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: err.message });
+    }
+  }
+
+  // Ici, je récupère tous les likes pour un post spécifique.
+  static async findByPostId(req, res) {
+    try {
+      const { postId } = req.params;
+      const likes = await LikeModel.findAll({ where: { postId } });
+      res.status(200).send(likes);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ error: err.message });
+    }
+  }
 }
 
 module.exports = LikeController;
