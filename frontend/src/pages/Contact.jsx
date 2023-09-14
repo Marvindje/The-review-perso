a4bedd61809ca665bdf4979c051b108069a0bfbe
Marvindje <marvindje@wilder.school>
2023-09-14 09:36:48 +0200

    probleme de footer et navbar dans contact resolu

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import InputField from '../components/InputField'; 
+import InputField from '../components/InputField';
+import Navbar from '../components/Navbar'; // Import du composant Navbar
+import Footer from '../components/Footer'; // Import du composant Footer
+
 function Contact() {
   return (
-    <div>
-      <InputField />
+    <div className="flex flex-col min-h-screen">
+      <Navbar /> {/* Utilisation du composant Navbar */}
+      <div className="flex-grow">
+        <InputField />
+      </div>
+      <Footer /> {/* Utilisation du composant Footer */}
     </div>
   );
 }
