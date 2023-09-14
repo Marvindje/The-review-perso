da43d68875a5148aaad06f873d7a6f4d8425ead9
Marvindje <marvindje@wilder.school>
2023-09-14 16:21:54 +0200

    fonction youtube dans mesposts

--- frontend/src/pages/MesPosts.jsx
+++ frontend/src/pages/MesPosts.jsx
@@ -150,9 +150,9 @@ function MesPosts() {
                 <iframe
                   width="560"
                   height="315"
-                  src={`https://www.youtube.com/embed/${renderYoutubeEmbed(
+                  src={`https://www.youtube.com/embed/${
                     post.youtubeLink
-                  )}`}
+                  }`}
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
